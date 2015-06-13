6c09f6e9a72f3bc3b7c7549eac4579e8e021a5e8
maslow <wangfugen@126.com>
2015-06-14 03:08:50 +0800

    部分实现节点管理代码

--- node/node.go
+++ node/node.go
@@ -1,13 +1,61 @@
 package node
 
 import (
-//"github.com/maslow/xmemcache/hasher"
+	"github.com/maslow/xmemcache/hasher"
+	//"github.com/maslow/xmemcache/config"
+	"strconv"
 )
 
-type VirtualNodeSet map[uint32]string
-type RealNodeSet map[string]string
+//默认<复制个数>
+const defaultCopyCount = 50
 
-func GenerateVirtualNodeSet() *VirtualNodeSet {
+const (
+	NODE_STATUS_ACTIVE   = 0
+	NODE_STATUS_INACTIVE = 1
+)
 
-	return nil
+type Nodes struct {
+	realNodes    map[string]uint8
+	virtualNodes map[uint32]string
+}
+
+//生成虚拟节点
+func (n *Nodes) generateVirtualNodeSet() {
+	n.virtualNodes = make(map[uint32]string)
+	for k, _ := range n.realNodes {
+		for i := 0; i < defaultCopyCount; i++ {
+			vnKey := k + "#" + strconv.Itoa(i)
+			hashValue := hasher.GetHashValue(vnKey)
+			n.virtualNodes[hashValue] = vnKey
+		}
+	}
+}
+
+//生成物理节点
+func (n *Nodes) generateRealNodeSet(servers []string) {
+	n.realNodes = make(map[string]uint8)
+	for _, v := range servers {
+		n.realNodes[v] = NODE_STATUS_ACTIVE
+	}
+}
+
+//初始化
+func (n *Nodes) Init(config []string) {
+	n.generateRealNodeSet(config)
+	n.generateVirtualNodeSet()
+}
+
+// TODO
+func (n *Nodes) To(key string) (addr string) {
+	return ""
+}
+
+// TODO
+func (n *Nodes) toVirtualNode(key string) (hashValue uint32) {
+	return 0
+}
+
+// TODO
+func (n *Nodes) toRealNode() (addr string) {
+	return ""
 }
--- node/node_test.go
+++ node/node_test.go
@@ -1,9 +1,44 @@
 package node
 
 import (
+	"fmt"
 	"testing"
 )
 
-func Test_GenerateVirtualNodeSet(t *testing.T) {
+func Test_generateReadNodeSet(t *testing.T) {
 
+}
+
+func Test_generateVirtualNodeSet(t *testing.T) {
+
+}
+
+//测试虚拟节点分散程度
+func Test_hitRate(t *testing.T) {
+	config := []string{"12.108.111.20", "19.168.3.61", "192.168.33.52", "192.168.333.13", "192.68.33.14", "12.168.33.15", "192.16.3.16"}
+	nodes := new(Nodes)
+	nodes.Init(config)
+
+	rnCount := uint32(len(nodes.realNodes))
+	var max uint32 = 0xFFFFFFFF
+
+	T := make([]uint32, rnCount)
+	for i := uint32(0); i < rnCount; i++ {
+		T[i] = (i + 1) * (max / rnCount)
+	}
+
+	C := make([]uint32, rnCount)
+
+	for k, _ := range nodes.virtualNodes {
+		for ii := uint32(0); ii < rnCount; ii++ {
+			if k <= T[ii] {
+				C[ii]++
+			}
+		}
+	}
+
+	for j := rnCount - 1; j > uint32(0); j-- {
+		C[j] = C[j] - C[j-1]
+	}
+	fmt.Println("虚拟节点分散情况:", C)
 }
